5c3cbb65bfd81ed3f66f335360f2227bafafe81a
Leonardo Gurgel <lgurgel@id.uff.br>
2020-06-11 22:57:23 -0300

    refactor main

--- main.rb
+++ main.rb
@@ -2,18 +2,17 @@ require_relative './boot'
 
 client = TwitterApi.new
 
-client.parsed_tweets.each do |string|
+tweets = client.parsed_tweets
+
+tweets.each do |string|
   begin
     text = Text.new string
     translation = Translator.translate text
     unless Logger.already_posted? translation.to_s
       Logger.mark_as_posted translation.to_s
-      puts 'postei'
-    else
-      puts 'já postado'
+      Logger.success text.to_s, translation.to_s
+      puts 'postar'
     end
-
-    Logger.success text.to_s, translation.to_s
   rescue NotImplementedError
     Logger.error text.to_s
   end
